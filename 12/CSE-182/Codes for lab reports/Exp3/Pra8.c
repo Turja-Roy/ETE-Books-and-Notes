b624fd219bedfae102ba6b4cd944819c7aa228b6
Turja Roy <turja.roy@outlook.com>
2023-07-24 03:08:41 +0600

    Update 2023-07-24 03:07

--- 12/CSE-182/Codes for lab reports/Exp3/Pra8.c
+++ 12/CSE-182/Codes for lab reports/Exp3/Pra8.c
@@ -10,22 +10,22 @@ int main()
     if ( unit <= 50 ){
         bill = unit * 0.5;
         bill += bill * (0.2);
-        printf("The electricity bill is Rs. %lf\n", bill);
+        printf("The electricity bill is Rs. %.2lf\n", bill);
     }
     else if ( unit <= 150 ){
         bill = 50*0.5 + (unit-50)*0.75;
         bill += bill * (0.2);
-        printf("The electricity bill is Rs. %lf\n", bill);
+        printf("The electricity bill is Rs. %.2lf\n", bill);
     }
     else if ( unit <= 250 ){
         bill = 50*0.5 + 100*0.75 + (unit-150)*1.2;
         bill += bill * (0.2);
-        printf("The electricity bill is Rs. %lf\n", bill);
+        printf("The electricity bill is Rs. %.2lf\n", bill);
     }
     else {
         bill = 50*0.5 + 100*0.75 + 100*1.2 + (unit-250)*1.5;
         bill += bill * (0.2);
-        printf("The electricity bill is Rs. %lf\n", bill);
+        printf("The electricity bill is Rs. %.2lf\n", bill);
     }
 
     return 0;
