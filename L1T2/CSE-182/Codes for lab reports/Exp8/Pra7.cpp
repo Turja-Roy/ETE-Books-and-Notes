caa3b7efd9ce9e6b6a5c89bc9f2dc8b70bfa1c6f
Turja Roy <turja.roy@outlook.com>
2023-09-04 07:49:50 +0600

    Update 2023-09-04 07:49

---
+++ L1T2/CSE-182/Codes for lab reports/Exp8/Pra7.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main ()
+{
+    int r, c;
+
+    cout << "Enter number of rows of the matrix: "; cin >> r;
+    cout << "Enter number of columns of the matrix: "; cin >> c;
+    vector<vector<int>> A(r, vector<int> (c));
+
+    cout << "Enter the matrix:" << endl;
+    for (int i=0; i<r ; i++) {
+        for (int j=0; j<c; j++) cin >> A[i][j];
+    }
+
+    if ( r != c ) {
+        cout << "Can\'t find lower triangle, the matrix isn\'t square" << endl;
+    } 
+
+    cout << "The lower triangle of the matrix:" << endl;
+    for (int i=0; i<r; i++) {
+        for (int j=0; j<c; j++) {
+            if ( j <= i ) cout << A[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ L1T2/CSE-182/Codes for lab reports/Exp8/Pra8.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main ()
+{
+    int r, c;
+
+    cout << "Enter number of rows of the matrix: "; cin >> r;
+    cout << "Enter number of columns of the matrix: "; cin >> c;
+    vector<vector<int>> A(r, vector<int> (c));
+
+    cout << "Enter the matrix:" << endl;
+    for (int i=0; i<r ; i++) {
+        for (int j=0; j<c; j++) cin >> A[i][j];
+    }
+
+    if ( r != c ) {
+        cout << "Can\'t find upper triangle, the matrix isn\'t square" << endl;
+    } 
+
+    int sum = 0;
+    for (int i=0; i<r; i++) {
+        for (int j=0; j<c; j++) {
+            if ( j >= i ) sum += A[i][j];
+        }
+    }
+
+    cout << "The sum of the upper triangular elements = " << sum << endl;
+
+    return 0;
+}
+
