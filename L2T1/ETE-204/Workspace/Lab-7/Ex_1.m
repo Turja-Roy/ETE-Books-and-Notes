02b314413aaae1be1dc1b8508b30defb48674849
Turja Roy <turja.roy@outlook.com>
2024-02-27 01:52:24 +0600

    Update 2024-02-27 01:52

--- L2T1/ETE-204/Workspace/Lab-7/Ex_1.m
+++ L2T1/ETE-204/Workspace/Lab-7/Ex_1.m
@@ -2,14 +2,14 @@ pkg load symbolic
 
 syms f(x) g(x);
 
-DE1 = DE1 = diff(f(x), x) + 1.5*f(x) - g(x) == 0;
-DE2 = DE2 = diff(g(x), x) + f(x) - 2*g(x) == 0;
+DE1 = diff(f(x), x) + 1.5*f(x) - g(x) == 0;
+DE2 = diff(g(x), x) + f(x) - 2*g(x) == 0;
 
 ode_sys = [DE1, DE2];
 
-soln = dsolve(ode_sys);
+soln = dsolve(ode_sys, [f(0) == 0, g(0) == 2]);
 
 disp('f(x) = ');
 pretty(soln.f)
-disp('g(x) = ')
-pretty(soln.g);
+disp('g(x) = ');
+pretty(soln.g)
